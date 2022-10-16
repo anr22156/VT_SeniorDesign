6ce89554dfe0977a3fc05db6e192e4df98d10539
Alexis Richmond <107123652+anr22156@users.noreply.github.com>
2022-10-16 16:06:21 -0400

    Add files via upload

---
+++ GITHUB_uploads/PPG_download_code.ps1
@@ -0,0 +1,13 @@
+﻿$link = "https://physionet.org/files/pulse-transit-time-ppg/1.1.0/csv/"
+$fileslink = Invoke-WebRequest -Uri "$link"
+$html = $fileslink.Content
+
+
+$ProgressPreference = 'SilentlyContinue'
+foreach ($line in $html.split("")) {
+
+if ($line.indexOf("href=") -ge 0) {
+    $file = $line.Substring( ("href=").Length + 1, $line.Length - $line.indexOf('">') - 6)
+    Invoke-WebRequest -Uri "$link$file" -OutFile "C:\Users\nitar\Downloads\files\csv\$file"
+}
+}
